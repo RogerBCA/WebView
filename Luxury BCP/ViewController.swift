7940a01e81022675db8f9b30dffe2671da549168
Roger Baltazar Cruz Alvarez <rbaltazarc@gmail.com>
2015-06-20 01:25:04 -0500

    Initial Commit

---
+++ Luxury BCP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Luxury BCP
+//
+//  Created by Roger Baltazar Cruz Alvarez on 20/06/15.
+//  Copyright (c) 2015 Roger Baltazar Cruz Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
